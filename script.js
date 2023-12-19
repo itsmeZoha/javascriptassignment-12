27740fd70a655e0d1cc392f0e7a9004f6ee3faa9
Zoha Asghar <callmezoiii42@gmail.com>
2023-12-20 00:29:25 +0500

    update code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// let userName = prompt("Please Enter your name to visit our website? ") 
-// window.onload = function(){
-//    let message ="Good Day"
-// //    let firstName = prompt("Please Enter Your First Name?")
-// //    let lastName = prompt("Please Enter Your Last Name?")
-// //    let fullName = firstName +" "+ lastName
-//    let banger ="!"
-//    document.getElementById("userName").innerHTML = message + " " + userName+ banger
-// }
+let userName = prompt("Please Enter your name to visit our website? ") 
+window.onload = function(){
+   let message ="Good Day"
+//    let firstName = prompt("Please Enter Your First Name?")
+//    let lastName = prompt("Please Enter Your Last Name?")
+//    let fullName = firstName +" "+ lastName
+   let banger ="!"
+   document.getElementById("userName").innerHTML = message + " " + userName+ banger
+}
 function showOutput(){
     return document.getElementById('output').innerHTML
 }
